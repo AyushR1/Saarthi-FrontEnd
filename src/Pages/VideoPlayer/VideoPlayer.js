1a1f1ecb769c268275d14e81ab92b8f9b9922baa
AyushR1 <ayushrathore12501@gmail.com>
2023-03-11 01:46:12 +0530

    fix bug
    Signed-off-by: AyushR1 <ayushrathore12501@gmail.com>

--- src/Pages/VideoPlayer/VideoPlayer.js
+++ src/Pages/VideoPlayer/VideoPlayer.js
@@ -1,12 +1,22 @@
 import React from "react";
 import RenderWithoutTracking from "./RenderWithoutTracking";
 import RenderWithTracking from "./RenderWithTracking";
-import { useSearchParams } from "react-router-dom";
+import { useLocation } from "react-router-dom";
+const VideoPlayer = ({ location }) => {
+  let { state } = useLocation();
+  let playlistID = state.playlistID;
+  if (playlistID === undefined) {
+    playlistID = localStorage.getItem("playlist-id");
+  } else {
+    localStorage.setItem("playlist-id", playlistID);
+  }
 
-const VideoPlayer = () => {
-  const [searchParams] = useSearchParams();
-  const playlistID = searchParams.get("playlistID") || localStorage.getItem("playlist-id");
-  const tracking = searchParams.get("tracking") || localStorage.getItem("tracking");
+  let tracking = state.tracking;
+  if (tracking === undefined) {
+    tracking = localStorage.getItem("tracking");
+  } else {
+    localStorage.setItem("tracking", tracking);
+  }
 
   if (tracking) {
     return <RenderWithTracking playlistID={playlistID} />;
