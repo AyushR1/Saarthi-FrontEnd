2d72e39a76c4a65e376afb69f73447f14ed7ff98
AyushR1 <ayushrathore12501@gmail.com>
2023-03-11 22:26:24 +0530

    App.js: Make code more understandable
    Signed-off-by: AyushR1 <ayushrathore12501@gmail.com>

--- src/App.js
+++ src/App.js
@@ -13,15 +13,24 @@ import SearchPage from "./Pages/Dashboard/SearchPage"
 import LandingHomePage from "./Pages/Dashboard/LandingHomePage";
 import { UserContext } from "./UserContext";
 
+// Create an instance of the Firebase authentication service.
 const auth = firebase.auth();
 
+/**
+ * The main component of the application.
+ */
 function App() {
+  // Define a state variable for the user ID.
   const [uid, setUid] = useState("");
+  // Create a context value for the user ID state.
   const value = { uid, setUid };
+
   return (
+    // Provide the user ID context to the application.
     <UserContext.Provider value={value}>
       <BrowserRouter>
         <Routes>
+          {/* Define the different routes of the application */}
           <Route exact path="/" element={<LandingHomePage />} />
           <Route path={"/dashboard"} element={<Home />} />
           <Route path={"/explore"} element={<ExplorePage />} />
@@ -35,14 +44,18 @@ function App() {
   );
 }
 
+/**
+ * The main content of the application, which is either the dashboard or the landing page.
+ */
 function Home() {
+  // Determine if a user is logged in.
   const [userLoggedIn] = useAuthState(auth);
 
-
+  // Render the appropriate content based on the user's login status.
   return (
     userLoggedIn ? <Dashboard /> : <LandingPage />
   );
 }
 
-
+// Export the main application component.
 export default App;
